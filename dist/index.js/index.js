b25591a00df100bb23b64d1e71389d3216901484
MissFlower <362554452@qq.com>
2021-10-25 23:32:16 +0800

    Delete dist/index.js directory

--- dist/index.js/index.js
+++
@@ -1,9 +0,0 @@
-// node_modules/ady-demo-a/src/index.js
-"use strict";
-function createName(name1, name2) {
-  return name1 + name2 + " end2";
-}
-var src_default = createName;
-
-// src/index.js
-console.log(src_default(1, 2));
