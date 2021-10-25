d3bf22cbcb907ef5c06abe1596f160d70e5efc4a
DoveyLoveyCora <DoveyLoveyCora@163.com>
2021-10-25 23:34:53 +0800

    feat:commit

--- packages/demo-b/node_modules/demo-a/src/index.js
+++ packages/demo-b/node_modules/demo-a/src/index.js
@@ -4,11 +4,11 @@
  * @Author: AiDongYang
  * @Date: 2021-10-25 21:07:21
  * @LastEditors: AiDongYang
- * @LastEditTime: 2021-10-25 23:15:22
+ * @LastEditTime: 2021-10-25 23:34:48
  */
 'use strict'
 
 function createName(name1, name2) {
-  return name1 + name2 + ' end2'
+  return name1 + name2 + '!!!'
 }
 export default createName
